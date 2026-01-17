4890683aad7784bffa644387131c15f49700acca
waterisbluexd <waterisblue275@gmail.com>
2026-01-17 08:02:00 +0530

    working

--- src/panels/right_main_panels.rs
+++ src/panels/right_main_panels.rs
@@ -136,7 +136,7 @@ pub fn right_main_panels_view<'a>(
                             container(
                                 button(
                                     container(
-                                        text(if control_center_visible { "" } else { "󰐥" })
+                                        text(if control_center_visible { "" } else { "" })
                                             .color(theme.color2)
                                             .font(font)
                                             .size(font_size * 1.3)
@@ -305,7 +305,7 @@ pub fn right_main_panels_view<'a>(
                         container(
                             button(
                                 container(
-                                    text("󰐥")
+                                    text("")
                                         .color(theme.color2)
                                         .font(font)
                                         .size(font_size * 1.3)
